09a81f575cf227eea0adb8c5558be1a66ff1ee3d
Esraa Mosaad <esraa.m.mosaad@gmail.com>
2025-05-27 06:04:57 +0300

    add dependencies needed

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,6 +2,8 @@ plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
+    id("com.google.devtools.ksp")
+    kotlin("plugin.serialization") version "2.1.10"
 }
 
 android {
@@ -41,11 +43,61 @@ android {
 
 dependencies {
 
+    testImplementation ("androidx.test:core-ktx:1.5.0")
+    testImplementation ("androidx.test.ext:junit-ktx:1.1.5")
+    testImplementation ("org.robolectric:robolectric:4.11")
+
+
+    val archTestingVersion = "2.2.0"
+    val coroutinesVersion = "1.7.3"
+    testImplementation("junit:junit:4.13.2")
+
+    // Dependencies for local unit tests
+    testImplementation("androidx.arch.core:core-testing:$archTestingVersion")
+
+    // hamcrest
+    testImplementation("org.hamcrest:hamcrest:2.2")
+    testImplementation("org.hamcrest:hamcrest-library:2.2")
+    androidTestImplementation("org.hamcrest:hamcrest:2.2")
+    androidTestImplementation("org.hamcrest:hamcrest-library:1.3")
+
+    //kotlinx-coroutines
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:$coroutinesVersion")
+    testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-test:$coroutinesVersion")
+
+    //MockK
+    testImplementation("io.mockk:mockk-android:1.13.17")
+    testImplementation("io.mockk:mockk-agent:1.13.17")
+    implementation("com.google.accompanist:accompanist-systemuicontroller:0.28.0")
+    implementation("network.chaintech:kmp-date-time-picker:1.0.7")
+    implementation("androidx.work:work-runtime-ktx:2.7.1")
+    implementation("com.airbnb.android:lottie-compose:6.6.3")
+
+    val navVersion = "2.8.8"
+    implementation("androidx.navigation:navigation-compose:$navVersion")
+
+    //Serialization for NavArgs & JetPack Components
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.8.0")//done
+    implementation("androidx.lifecycle:lifecycle-viewmodel-compose-android:2.8.7")
+    val composeVersion = "1.0.0"
+    implementation("androidx.compose.runtime:runtime-livedata:$composeVersion")
+    implementation("androidx.work:work-runtime-ktx:2.10.0")
+
+    //Retrofit & Media
+    implementation("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation("com.google.code.gson:gson:2.10.1")
+    implementation("com.squareup.retrofit2:converter-gson:2.9.0")
+    implementation("io.coil-kt:coil-compose:2.7.0")
+
+    //Room Database
+    implementation("androidx.room:room-ktx:2.6.1")
+    implementation("androidx.room:room-runtime:2.6.1")
+    ksp("androidx.room:room-compiler:2.6.1")
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
     implementation(platform(libs.androidx.compose.bom))
-    implementation(libs.androidx.ui)
+    implementation(libs.androidx.ui)//done
     implementation(libs.androidx.ui.graphics)
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
